06c5490fbc0f550616e840c8eec6db130f3b2fb5
RMP <ruthiepina@gmail.com>
2023-11-21 00:03:43 -0700

    Started code for displaying time blocks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,6 @@ $(function () {
    // time-block containing the button that was clicked? How might the id be
    // useful when saving the description in local storage?
    //
-   var today = dayjs();
-   var formatDate = dayjs(today).format("dddd, MMMM D YYYY, h:mm a");
-   $("#currentDay").text(formatDate); //* document.getElementById("currentDay").textContent = formatDate;
 
    // TODO: Add code to apply the past, present, or future class to each time
    // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,9 +17,21 @@ $(function () {
    // past, present, and future classes? How can Day.js be used to get the
    // current hour in 24-hour time?
    //
+   for (var hour = 9; hour < 17; hour++) {
+      var iconEl = $('<i class="fas fa-save" aria-hidden="true">');
+      var buttonEl = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">');
+      var textAreaEl = $('<textarea class="col-8 col-md-10 description" rows="3">');
+      var divHourDisplayEl = $('<div class="col-2 col-md-1 hour text-center py-3">');
+      var divHourEl = $('<div class="row time-block">');
+      console.log("file: script.js:26 ~ divHourEl:", divHourEl);
+   }
    // TODO: Add code to get any user input that was saved in localStorage and set
    // the values of the corresponding textarea elements. HINT: How can the id
    // attribute of each time-block be used to do this?
    //
    // TODO: Add code to display the current date in the header of the page.
+   //* Adds current date to header
+   var today = dayjs();
+   var formatDate = dayjs(today).format("dddd, MMMM D YYYY, h:mm a");
+   $("#currentDay").text(formatDate); //* document.getElementById("currentDay").textContent = formatDate;
 });
