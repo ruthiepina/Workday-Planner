3196db185958d681de97b0c43506640170989c37
RMP <ruthiepina@gmail.com>
2023-12-03 16:09:20 -0700

    Added event listener and handler to save tasks to local storage. Refactored and cleaned code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,29 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-$(function () {
-   // TODO: Add code to display the current date in the header of the page.
+//* Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+//* the code isn't run until the browser has finished rendering all the elements
+//* in the html.
+$(() => {
    //* Adds current date to header
-   var today = dayjs();
-   var formatDate = dayjs(today).format("dddd, MMMM D YYYY, h:mm a");
-   $("#currentDay").text(formatDate); //* document.getElementById("currentDay").textContent = formatDate;
+   let today = dayjs();
+   let formatDate = dayjs(today).format("dddd, MMMM D YYYY, h:mm a");
+   $("#currentDay").text(formatDate);
 
-   // TODO: Add a listener for click events on the save button. This code should
-   // use the id in the containing time-block as a key to save the user input in
-   // local storage. HINT: What does `this` reference in the click listener
-   // function? How can DOM traversal be used to get the "hour-x" id of the
-   // time-block containing the button that was clicked? How might the id be
-   // useful when saving the description in local storage?
-   //
-
-   // TODO: Add code to apply the past, present, or future class to each time
-   // block by comparing the id to the current hour. HINTS: How can the id
-   // attribute of each time-block be used to conditionally add or remove the
-   // past, present, and future classes? How can Day.js be used to get the
-   // current hour in 24-hour time?
-   //
-   //* Variable to hold info to localStorage
-   var tasks = [];
-
-   //* Check for any current or previous data located in localStorage
+   let tasks = []; //* Variable to hold info to localStorage
+   //* Check for any previous data located in localStorage
    tasks = JSON.parse(localStorage.getItem("myDay"));
    if (!tasks) {
-      //* If some data is found, then store that in tasks array
+      //* If there is no data, then prepare tasks array
       tasks = [];
    }
-   console.log("file: script.js:34 ~ tasks:", tasks);
 
+   //* Displays the workday planner
    const NOON = 12;
-   for (let hour = 0; hour < 24; hour++) {
+   const DAY_START = 9;
+   const DAY_END = 18;
+   for (let hour = DAY_START; hour < DAY_END; hour++) {
       let currentHour = dayjs().hour();
-      let classColor = "";
 
+      //* Color code time blocks
+      let classColor = "";
       if (currentHour > hour) {
          classColor = "past";
       } else {
@@ -49,20 +34,28 @@ $(function () {
          }
       }
 
+      //* Creates time blocks
       let hourId = "hour-" + hour;
       $("<div id='" + hourId + "' class='row time-block " + classColor + "'></div>").appendTo("#cont-fluid");
       $("<div id='hour-text-" + hour + "' class='col-2 col-md-1 hour text-center py-3'></div>").appendTo("#" + hourId);
+      $("<textarea id='textarea-" + hour + "' class='col-8 col-md-10 description' rows='3'></textarea>").appendTo("#" + hourId);
+      $("<button id='btn-" + hour + "' class='btn saveBtn col-2 col-md-1' aria-label='save'></button>").appendTo("#" + hourId);
+      $("<i id='icon-" + hour + "' data-hour-index='" + hour + "' class='fas fa-save' aria-hidden='true'></i>").appendTo("#btn-" + hour);
 
+      //* Determines morning/evening time block
       let displayTime = hour;
       displayTime -= hour > NOON ? NOON : 0;
       displayTime += hour < NOON ? " AM" : " PM";
       $("#hour-text-" + hour).text(displayTime);
 
-      $("<textarea id='textarea-" + hour + "' class='col-8 col-md-10 description' rows='3'></textarea>").appendTo("#" + hourId);
       //* Refresh planner from local storage
       $("#textarea-" + hour).val(tasks[hour]);
-
-      $("<button id='btn-" + hour + "' class='btn saveBtn col-2 col-md-1' aria-label='save'></button>").appendTo("#" + hourId);
-      $("<i id='icon-" + hour + "' class='fas fa-save' aria-hidden='true'></i>").appendTo("#btn-" + hour);
    }
+
+   //* Event listener to save task to planner (bubbling)
+   $("#cont-fluid").on("click", "i", (event) => {
+      let hourIndex = $(event.target).data("hour-index");
+      tasks[hourIndex] = $("#textarea-" + hourIndex).val(); //* Updates array from textarea
+      localStorage.setItem("myDay", JSON.stringify(tasks)); //* Updates localStorage
+   });
 });
